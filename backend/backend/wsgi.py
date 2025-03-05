0ca25a6a2e26b1b49eb0fa138afc95682f96c9bf
Rutuja Kodag <kodagrutuja0@gmail.com>
2025-03-05 18:56:53 +0530

    updated manage.y and wsgi.py

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
